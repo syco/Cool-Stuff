20f2a375c3b0919bff6c9f6a094c1b69b1d8ba36
Syco <alberto.rinaudo+torann@gmail.com>
2021-02-18 17:07:00 +0000

    add local streamlink
    Signed-off-by: Syco <alberto.rinaudo+torann@gmail.com>

---
+++ _build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for x in "plugin.video.italy-tv" "plugin.video.uk-tv" "script.module.streamlink.base"; do
+  rm -Rf ${x}*
+  curl -s -L https://github.com/syco/${x}/archive/master.zip > ${x}.zip
+  unzip ${x}.zip
+  mv ${x}-master ${x}
+  rm -f ${x}.zip
+done
+python3 _repo_xml_generator.py
+for x in "plugin.video.italy-tv" "plugin.video.uk-tv" "script.module.streamlink.base"; do
+  rm -Rf ${x}*
+done
+
