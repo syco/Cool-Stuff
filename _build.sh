3e3f58187e18790e079503f52c712b30433b7bd8
Alberto <alberto.rinaudo+dino@gmail.com>
2021-11-15 23:22:07 +0000

    add script.module.streamlink
    Signed-off-by: Alberto <alberto.rinaudo+dino@gmail.com>

--- _build.sh
+++ _build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for x in "plugin.video.italy-tv" "plugin.video.uk-tv"; do
+for x in "plugin.video.italy-tv" "plugin.video.uk-tv" "script.module.streamlink"; do
   rm -Rf ${x}*
   curl -s -L https://github.com/syco/${x}/archive/master.zip > ${x}.zip
   unzip ${x}.zip
@@ -8,7 +8,7 @@ for x in "plugin.video.italy-tv" "plugin.video.uk-tv"; do
   rm -f ${x}.zip
 done
 python3 _repo_xml_generator.py
-for x in "plugin.video.italy-tv" "plugin.video.uk-tv"; do
+for x in "plugin.video.italy-tv" "plugin.video.uk-tv" "script.module.streamlink"; do
   rm -Rf ${x}*
 done
 
